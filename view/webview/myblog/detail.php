1a7888f9cfc0885dafe8b9f7f485b2361d606734
mylu <290162743@qq.com>
2020-12-07 23:55:17 +0800

    php练习

--- view/webview/myblog/detail.php
+++ view/webview/myblog/detail.php
@@ -6,244 +6,10 @@
     <title>迷失的路</title>
     <link rel="shortcut icon" type="image/x-icon" href="../static/images/web-icon.png" media="screen" />
     <link rel="stylesheet" href="../static/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../static/css/article.css">
     <script src="../static/js/jquery-3.2.1.min.js"></script>
     <script src="../static/js/bootstrap.min.js"></script>
-    <style>
-
-        *{
-            padding: 0;
-            margin: 0;
-        }
-        #bg:after {
-            content: "";
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background: rgba(0,0,0,.4);
-            z-index: 1;
-        }/*给背景图片设置蒙版*/
-        body {
-            font: 14px/1.5 微软雅黑, arial, sans-serif;
-            width: 100%;
-            overflow: scroll;
-        }
-        #header {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 66px;
-            color: #fff;
-            z-index: 9999;
-            transition: all .3s;
-        }/*设置页眉*/
-        #header nav {
-            position: relative;
-            width: 80%;
-            margin: 0 auto;
-        }
-        ul {
-            list-style: none;
-        }/*去掉列表样式*/
-        #header nav li {
-            border: 0;
-            display: inline-block;
-            height: 66px;
-            line-height: 66px;
-            font-size: 16px;
-            position: relative;
-            cursor: pointer;
-        }
-        #header nav li a {
-            display: inline-block;
-            padding: 0 12px;
-            white-space: nowrap;
-            color: #fff;
-        }
-        a {
-            text-decoration: none;
-        }/*去掉链接下划线*/
-        #bg {
-            position: relative;
-            background: url("./static/images/mybackground.jpg") no-repeat gray;
-            background-size: cover;
-        }/*设置背景图片*/
-        #bg p {
-            position: relative;
-            color: hsla(0,0%,100%,.9);
-            text-align: center;
-            font-size: 26px;
-            line-height: 2;
-            z-index: 2;
-            letter-spacing: 3px;
-            text-shadow: 0 3px 25px rgba(0,0,0,.3);
-            cursor: progress;
-            padding: 210px 0px;
-        }/*设置文字属性*/
-        #bg p i {
-            text-decoration: line-through;
-            font-size: 20px;
-            color: hsla(0,0%,100%,.8);
-            text-shadow: 0 0 0 transparent;
-            font-style: normal;
-        }/*设置第二行文字属性*/
-        #header nav figure
-        {
-            background:url("");
-            height: 50px;
-            width: 50px;
-            background-size:cover ;
-
-        }
-        #header nav .my-info {
-            position: absolute;
-            top: 8px;
-            right: 0;
-        }
-        figure {
-            display: block;
-            -webkit-margin-before: 1em;
-            -webkit-margin-after: 1em;
-            -webkit-margin-start: 40px;
-            -webkit-margin-end: 40px;
-            border-radius:100px;
-        }
-
-        #header nav span {
-            font-size: 18px;
-            margin: 0 10px;
-        }
-        #header nav figure, #header nav span {
-            display: inline-block;
-
-            vertical-align: middle;
-        }
-
-        #copyright {
-            position: absolute;
-            top: 0;
-            left: 0;
-            background: #0e0e0e;
-            width: 100%;
-            height: 100%;
-            transform-origin: 100% 0;
-            transition: all .5s;
-            z-index: 2;
-        }
-        #copyright p {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%,-50%);
-            line-height: 2;
-        }
-        #footer {
-            position: relative;
-            height: 160px;
-            border-top: 1px solid #eee;
-            color: hsla(0,0%,100%,.69);
-            font-size: 12px;
-            text-align: center;
-            border-top: 2px dashed #98b7ff;
-            transition: height .1s 1s;
-        }
-        #hiddenewm p{
-            color: #444;
-        }
-        #hiddenewm
-        {
-            background: #fff;
-            text-align: center;
-            position: absolute;
-            left: 25px;
-            top: 70px;
-        }
-        #container {
-            position: relative;
-            max-width: 95%;
-            margin: 0 auto;
-        }
-        .article {
-            box-shadow: 5px 5px 25px #dadada;
-            position: relative;
-            padding: 0 20px;
-            margin: 40px auto;
-            max-width: 950px;
-            background: #fff;
-            text-align: center;
-        }
-        .article>time {
-            position: absolute;
-            top: 0;
-            left: 0;
-            border-bottom: 1px solid #ccc;
-            font-size: 14px;
-            padding: 4px 5px 0;
-            color: #999;
-        }
-        .article h2 {
-            padding-bottom: .5em;
-            font-size: 1.75em;
-            line-height: 1.225;
-        }
-        .article>h2 {
-            padding: 35px 0 45px;
-            font-size: 22px;
-            font-weight: 700;
-            cursor: pointer;
-        }
-        #container a {
-            color: #333;
-        }
-        .article>span {
-            position: absolute;
-            top: 0;
-            right: 0;
-            color: #999;
-            padding: 3px 10px;
-            background: #f1f1f1;
-            font-size: 14px;
-        }
-        .article section {
-            text-align: left;
-            padding: 10px;
-            font-size: 16px;
-        }
-        .article blockquote {
-            padding: 0 15px;
-            color: #777;
-            border-left: 4px solid #ddd;
-        }
-        .article footer {
-            padding: 25px 0 20px;
-        }
-        .article footer a {
-            display: inline-block;
-            color: #18bc9c;
-            cursor: pointer;
-            padding: 4px 20px;
-            border-radius: 5px;
-            transition: all .5s;
-            border: 1px solid #18bc9c;
-        }
-        #pagebar{
-            text-align: center;
-        }
-        .comment{
-        width:60%;
-        position:relative;
-        left:20%;
-        border:1px ;
-        text-align:center;
-        margin:9px;
-        background-color:#fff;
-        border-radius: 5px;
-        transition: all .5s;
-        }
-
-    </style>
+    <script src="../static/js/layer.js"></script>
 </head>
 <body background="../static/images/bg.png" >
 
@@ -279,6 +45,14 @@
 <?php
 include("../config.php"); 
 $id=$_GET['id'];
+if(isset($_GET['reference']))
+$reference=$_GET['reference'];
+else
+$reference=0;
+if(isset($_GET['floor']))
+$floor=$_GET['floor'];
+else
+$floor=0;
 $user_id=$_SESSION['user_id'];
 $result=$conn->query("select * from article where id=".$id); 
 $article=mysqli_fetch_assoc($result);
@@ -310,7 +84,20 @@ $article=mysqli_fetch_assoc($result);
     $i=1;
     $comments=$conn->query("select * from comment  where article_id='".$id."' order by id");  
    foreach($comments  as $k => $v):?>
-        <article class="comment">
+        <article class="comment" id="<?php echo "comment_".$v['id'].","."floor_".$i;?>">
+                <?php
+                if($v['reference'])
+                {
+                    ?>
+                <section style="text-align:left">
+                <?php $result4=$conn->query("select * from comment where id=".$v['reference']);
+                      $ref=mysqli_fetch_assoc($result4);
+                ?>
+                回复<?php echo $ref['floor'];  ?>楼&nbsp;&nbsp;<?php echo $ref['nickname']; ?>&nbsp;&nbsp;<?php echo $ref['date']; ?><br/><br/>
+                <p><?php echo $ref['content']; ?></p><br/>
+                </section>
+                <?php
+            }?>
                 <section style="text-align:left">
                 <?php echo $i++;  ?>楼&nbsp;&nbsp;<?php echo $v['nickname']; ?>&nbsp;&nbsp;<?php echo $v['date']; ?><br/><br/>
                 <p><?php echo $v['content']; ?></p><br/>
@@ -322,9 +109,11 @@ $article=mysqli_fetch_assoc($result);
    <form id="myform" action="doaddcomment.php" method="post">
    <input type="hidden" name="article_id" value="<?php echo $id;?>">
    <input type="hidden" name="flag" id="flag" value="true">
+   <input id="reference" type="hidden" name="reference" value="<?php echo $reference; ?>">
+   <input id="floor" type="hidden" name="floor" value="<?php echo $i; ?>">
     		<div class="form-horizontal" role="form" style="margin:10px">
     			<div class="form-group">
-                        <label for="inputPassword" class="col-sm-2 control-label">评论</label>
+                        <label for="inputPassword" class="col-sm-2 control-label"><?php if($reference)echo"回复第".$floor."楼：";else echo "评论";?></label>
                         <div class="col-sm-3">
                                <textarea name="content" id="content"  class="form-control" rows="3"  placeholder="文明上网，理性发言" ></textarea>
                          </div>
@@ -339,7 +128,38 @@ $article=mysqli_fetch_assoc($result);
     			</div>
                 </form>
     			 <script>
-
+                        $('article').each(function(){
+                            $(this).click(function(){
+                                str=$(this).attr('id');
+                                str=str.replace("comment_","");
+                                str=str.replace("floor_","");
+                                var arr=new Array();
+                                var arr=str.split(",");
+                                if(arr[0]==<?php echo "".$reference;?>)
+                                {
+                                    layer.confirm('确定取消回复该条评论吗?', {
+                         btn: ['确定','取消'] 
+                   }, function(){
+                    var url="detail.php?id=<?php echo $id;?>";
+                       location.replace(url)
+      
+                      }, function(){
+                       
+                      });
+                                }
+                                else{
+                                layer.confirm('确定回复该条评论吗?', {
+                         btn: ['确定','取消'] 
+                   }, function(){
+                      var url="detail.php?id=<?php echo $id;?>&reference="+arr[0]+"&floor="+arr[1];
+                       location.replace(url)
+      
+                      }, function(){
+                      });
+                            }
+                        }
+                            )
+                        })
                         $("#commentButton").click(function () {
                            <?php 
                            if(!$user_id)
--- view/webview/myblog/doaddcomment.php
+++ view/webview/myblog/doaddcomment.php
@@ -3,6 +3,8 @@ include("../config.php");
 $article_id=$_POST['article_id'];
 session_start();
 $flag=$_POST['flag'];
+$floor=$_POST['floor'];
+$reference=$_POST['reference'];
 $user_id=$_SESSION['user_id'];
 $result=$conn->query("select * from user where id=".$user_id);
 $user=mysqli_fetch_assoc($result);
@@ -12,5 +14,5 @@ $content=$_POST['content'];
 date_default_timezone_set('PRC');
 $date=date('Y-m-d H:i:s',time());
 if($flag=="true")
-$result=$conn->query("insert into comment(article_id,content,date,name,nickname) values('$article_id','$content','$date','$name','$nickname')");
+$result=$conn->query("insert into comment(article_id,content,date,name,nickname,reference,floor) values('$article_id','$content','$date','$name','$nickname','$reference','$floor')");
 header("location:./detail.php?id=".$article_id);
